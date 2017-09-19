5da980525b53bb17fed0bad628c4e88da33f1f14
Aditya Murda <aditya_murda@yahoo.co.id>
2017-09-19 13:41:45 +0700

    update; bug pengiriman solved

--- burner/src/main.cpp
+++ burner/src/main.cpp
@@ -7,10 +7,24 @@ void setup()
 {
 	EEPROM.begin();
 	address = ((uint16_t) EEPROM.read(0x0ffe) << 8) | EEPROM.read(0x0fff);
+	// Serial.setTimeout(500000);
 	Serial.begin(9600);
 	Serial.print("Current address: "); Serial.println(address, OCT);
 	Serial.print("Enter new address: ");
+	String nums = Serial.readString();
+	// char nums_buf[15];
+	// nums.toCharArray(nums_buf, 15);
+	// address = atoi(nums_buf);
+
+	// wait for input
+	while(!Serial.available())
+		;
+
 	address = Serial.parseInt();
+	Serial.print("New address is "); Serial.println(address, OCT);
+	Serial.print("Write address? (press enter to write)");
+	while(!Serial.available())
+		;
 	EEPROM.write(0x0fff, address | 8);
 	EEPROM.write(0x0ffe, (address >> 8));
 	Serial.print("New address: ");
